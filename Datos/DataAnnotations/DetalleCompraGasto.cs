4614298a1be50135a05b131e7a5aa9205bd9d62b
JeffersonCuji96 <renemarin96jc@gmail.com>
2022-03-01 12:00:44 -0500

    Se agrega DB local, clase vista-modelo y dataannotations del detalle de compra y gasto

---
+++ Datos/DataAnnotations/DetalleCompraGasto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Datos.DataAnnotations
+{
+    public class DetalleCompraGasto
+    {
+        [Required(ErrorMessage = "Fecha requerida")]
+        public DateTime Fecha { get; set; }
+        public List<TempDetalleCompra> DetalleCompra { get; set; }
+        public List<TempDetalleGasto> DetalleGasto { get; set; }
+    }
+
+    public class TempDetalleCompra
+    {
+        [Required(ErrorMessage = "Producto requerido")]
+        public string Producto { get; set; }
+        
+        [Required(ErrorMessage = "Cantidad del producto requerido")]
+        [RegularExpression("^[0-9]+$", ErrorMessage = "Cantidad del producto de ser numérico")]
+        [Range(1, 100, ErrorMessage = "Cantidad del producto mínimo 1")]
+        public int Cantidad { get; set; }
+        
+        [Required(ErrorMessage = "Precio del producto requerido")]
+        [RegularExpression(@"^[0-9]+([,][0-9]{1,2})?$", ErrorMessage = "Precio del producto admite coma y 2 decimales")]
+        public string Precio { get; set; }
+    }
+    public class TempDetalleGasto
+    {
+        [Required(ErrorMessage = "Gasto requerido")]
+        public long IdGasto { get; set; }
+
+        [Required(ErrorMessage = "Precio del gasto requerido")]
+        [RegularExpression(@"^[0-9]+([,][0-9]{1,2})?$", ErrorMessage = "Precio del gasto admite coma y 2 decimales")]
+        public string Precio { get; set; }
+    }
+}
---
+++ Datos/ViewModels/CompraGastoViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Datos.ViewModels
+{
+    public class CompraGastoViewModel
+    {
+        public string Producto { get; set; }
+        public decimal Precio { get; set; }
+        public decimal TotalCompra { get; set; }
+    }
+    public class MontoCompraGasto
+    {
+        public decimal TotalGasto { get; set; }
+        public decimal PorcentajeCosto { get; set; }
+        public decimal TotalCosto { get; set; }
+    }
+}
